09014c4bb991970f5dd61b151e01cb9518746c57
SengMing Tan <tansengming@gmail.com>
2011-12-03 22:21:43 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FbAuthenticatable::Application.config.secret_token = 'f27e41785e52700ef48fdf72861003de301abf09baef88bd9d3799f95213495f03f52ad204056120b558a1eb124621c2c1e29543cf95d77713eddb5b1c0f6c94'
